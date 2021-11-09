386c41f5baee40a3809950a2c67657d444550341
Luis Enrique <enrique_inf@hotmail.com>
2021-11-09 17:53:34 -0600

    primer commit

---
+++ Consumo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.ServiciosSoapClient client = new ServiceReference1.ServiciosSoapClient();
+
+            //string result = client.HelloWorld();
+            //string result = client.Saludar("Enrique");
+            string result = client.GuardarLog("otro mensaje");
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
